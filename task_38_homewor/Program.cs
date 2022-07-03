2f09d0b74b3790c5e89247effb4ed51f88cab238
deardaddy <sergpomorsky@gmail.com>
2022-07-03 16:06:33 +0300

    add update task 38 homework

--- task_38_homewor/Program.cs
+++ task_38_homewor/Program.cs
@@ -2,22 +2,19 @@
 // // Найдите разницу между максимальным и минимальным элементом массива.
 // //3 7 22 2 78] -> 76
 double[] array = new double [4];
-//var array = new decimal[10];
-            for (int i = 0; i < array.Length; i++)
-            {
-                //Console.Write("Введите {0} число : ", i+1);
-                array[i] = new Random().Next(0, 10);
-                Console.Write($"{array[i]}, ");
-                //array[i] = Convert.ToDecimal(Console.ReadLine());
-            }
+for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 10);
+        Console.Write($"{array[i]}, ");
+    }
  
-            double max = array[0];
-            double min = array[0];
-            for (int i = 1; i < array.Length; i++)
-            {
-                if (array[i] < min) min = array[i];
-                if (array[i] > max) max = array[i];
-            }
-            
-            Console.WriteLine("Разность между максимальным и минимальным = {0}", max - min);
+    double max = array[0];
+    double min = array[0];
+    for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i] < min) min = array[i];
+            if (array[i] > max) max = array[i];
+        }
+    double sum = max - min;
+ Console.WriteLine($"Разность между максимальным и минимальным = {sum}");
         
